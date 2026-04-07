65568127627f63c4b4634d3de55ea956139e7372
EduardDGP <edugines964@gmail.com>
2026-04-07 15:44:38 +0200

    Notificaciones

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ import Social         from './pages/Social'
 import Perfil         from './pages/Perfil'
 import Admin          from './pages/Admin'
 import Alumnos        from './pages/Alumnos'
-import Notificaciones from './pages/notificaciones'
+import Notificaciones from './pages/Notificaciones'
 import Guardias       from './pages/Guardias'
 import Toast          from './components/shared/Toast'
 
